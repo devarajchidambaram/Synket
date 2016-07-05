cb261d02f96717b154779d0f52984f93b3462b46
Tom Butler <tom@r.je>
2016-07-05 13:21:30 +0100

    initial commit

---
+++ src/Synket.js
@@ -0,0 +1,27 @@
+var childProcess = require('child_process');
+
+module.exports = class {
+	constructor(socket) {
+		this.socket = socket;
+	}
+
+	startServer(module) {
+		var module = process.cwd() + '/' + module;
+		var result = childProcess.spawnSync('node', [__dirname + '/Client.js', 'start', JSON.stringify({args: [this.socket, module]})	]);
+	}
+
+	send(data, args) {
+		if (args) {
+			data = JSON.stringify({message: data, args: args});
+		}
+
+		var result = childProcess.spawnSync('node', [__dirname + '/Client.js', 'send', JSON.stringify({args: [this.socket, 
+												 data 
+												]})]);
+
+		if (new Buffer(result.stderr).toString().trim("\n").length > 0) {
+			throw new Error(new Buffer(result.stderr).toString());
+		}
+		return new Buffer(result.stdout).toString().trim("\n");
+	}
+}
